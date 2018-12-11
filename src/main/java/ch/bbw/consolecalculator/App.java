3e96db6b7b2ed0c2a3a542b1109f7471af2a5496
DennisRittmeyer <dennis.r@tamburin.ch>
2018-12-11 11:48:14 +0100

    Implement equivalence testclasses for division

--- src/main/java/ch/bbw/consolecalculator/App.java
+++ src/main/java/ch/bbw/consolecalculator/App.java
@@ -20,5 +20,6 @@ public class App
         valueA = 10;
         valueB = 20;
         System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(10, 20));
+        //System.out.println("test: " + Integer.MIN_VALUE + "  test  " +  Integer.MAX_VALUE);
     }
 }
